db86850b245036de43c5bdc58f9b22b6866fdd0e
Viorel Morari <viorell91@yahoo.com>
2016-06-25 14:27:53 +0200

    Add files via upload
    corrected "id" error

--- src/main/java/parser/CommentsParser.java
+++ src/main/java/parser/CommentsParser.java
@@ -2,8 +2,13 @@ package parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import datastructure.Comment;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  * Created by Shahbaz on 11/06/2016.
  */
@@ -12,28 +17,53 @@ public class CommentsParser {
 
     public static void main(String[] args) {
         String currentLine;
-        String pattern="tl;dr";
 
-        try(BufferedReader bufferedReader=new BufferedReader(new FileReader("D:\\Semester\\summarization\\comments-sample.ldjson"))){
-
-            File bodytext= new File("D:\\Semester\\summarization\\comments.ldjson");
+        try(BufferedReader bufferedReader=new BufferedReader(new FileReader("C:\\Users\\SpongeBob\\Downloads\\Sem III\\Big Data\\text-summarization-data-sample\\comments-sample.ldjson"))){
+        	
+        	File bodytext= new File("C:\\Users\\SpongeBob\\Downloads\\Sem III\\Big Data\\comments.jsonl");
             BufferedWriter bufferedWriter= new BufferedWriter(new FileWriter(bodytext));
-            bufferedWriter.write("");
+            
             ObjectMapper readMapper = new ObjectMapper();
+            ObjectMapper writeMapper = new ObjectMapper();
             JsonNode node;
             String content;
+            String commentExtract;
+            String tldr;
+            String id;
             int count=0;
-            while((currentLine=bufferedReader.readLine())!=null && count<200){
+            int invalid_comments_count = 0;
+            int invalid_tldr_count = 0;
+            List<String> invalid_comments = new ArrayList<String>();
+            List<String> invalid_tldr = new ArrayList<String>();
+   
+            Pattern c = Pattern.compile("(.+?)tl;dr", Pattern.DOTALL); //define pattern for matching tl;dr tags
+            while((currentLine=bufferedReader.readLine())!=null && count <200){
                 node= readMapper.readValue(currentLine,JsonNode.class);
-                content=node.get("body").asText();
-                if(content.toLowerCase().contains(pattern)){
-                    bufferedWriter.write("{ " +content+ " }");
-                    bufferedWriter.newLine();
+                id = node.get("id").asText();
+                content = node.get("body").asText().toLowerCase();
+                tldr = content.substring(content.indexOf("tl;dr")+5, content.length());
+                Matcher comment_match = c.matcher(content);
+                if (content.length() < 50) {
+					invalid_comments_count ++;
+					invalid_comments.add(id);
+					continue;
+				}
+                if (tldr.length() < 5) {
+					invalid_tldr_count ++;
+					invalid_tldr.add(id);
+					continue;
+				}
+                
+                if(comment_match.find() && comment_match.group(1).length() > 50){
+                	commentExtract = comment_match.group(1);
+                	Comment comment = new Comment(id,commentExtract,tldr);
+                	bufferedWriter.write(writeMapper.writeValueAsString(comment));
+	                bufferedWriter.newLine();
                 }
-
-                count++;
+               count++;
             }
-
+            System.out.println("Invalid comments: "+ invalid_comments_count +"\n"+"Invalid tldrs: "+ invalid_tldr_count);
+            System.out.println(invalid_comments.get(1));
             bufferedWriter.flush();
             bufferedWriter.close();
 
--- src/main/java/parser/SubmissionsParser.java
+++ src/main/java/parser/SubmissionsParser.java
@@ -3,12 +3,66 @@ package parser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import datastructure.Comment;
+
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Shahbaz on 11/06/2016.
  */
+
 public class SubmissionsParser {
+
+
+    public static void main(String[] args) {
+        String currentLine;
+
+        try(BufferedReader bufferedReader=new BufferedReader(new FileReader("C:\\Users\\SpongeBob\\Downloads\\Sem III\\Big Data\\text-summarization-data-sample\\submissions-sample.ldjson"))){
+        	
+        	File bodytext= new File("C:\\Users\\SpongeBob\\Downloads\\Sem III\\Big Data\\submissions.jsonl");
+            //BufferedWriter bufferedWriter= new BufferedWriter(new FileWriter(bodytext), "UTF8");
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(bodytext), "UTF8"));
+            ObjectMapper readMapper = new ObjectMapper();
+            ObjectMapper writeMapper = new ObjectMapper();
+            JsonNode node;
+            String content;
+            String submissionExtract;
+            String tldr;
+            String id;
+            int count=0;
+   
+            Pattern c = Pattern.compile("(.+?)tl;dr", Pattern.DOTALL); //define pattern for matching tl;dr tags
+            while((currentLine=bufferedReader.readLine())!=null && count < 200){
+                node = readMapper.readValue(currentLine,JsonNode.class);
+                id = node.get("id").asText();
+                content = node.get("selftext").asText().toLowerCase();
+                tldr = content.substring(content.indexOf("tl;dr")+5, content.length());
+                Matcher comment_match = c.matcher(content);
+                if(comment_match.find() && comment_match.group(1).length() > 10){
+                	submissionExtract = comment_match.group(1);
+                	Comment comment = new Comment(id,submissionExtract,tldr);
+                	bufferedWriter.write(writeMapper.writeValueAsString(comment));
+	                bufferedWriter.newLine();
+                }
+               count++;
+            }
+
+            bufferedWriter.flush();
+            bufferedWriter.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("File not readable");
+        }
+    }
+}
+
+
+
+/*public class SubmissionsParser {
     public static void main(String[] args) {
         String currentLine;
         String pattern="tl;dr";
@@ -42,3 +96,4 @@ public class SubmissionsParser {
         }
     }
 }
+*/
